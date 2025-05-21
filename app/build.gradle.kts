631988905bd9f5887a9def9fb5adf3de2d2c24be
Jhon Montero <jmontero@tpaga.co>
2025-05-20 20:14:25 -0500

    Agregado - Dependencia room-compiler v. 2.7.1 - Dependencia room-ktx v. 2.7.1 - Dependencia room-runtime v. 2.7.1 - PLugin kotlin-kapt v. 2.1.21
    Cambiado
    - Dependencia compose-bom actualizada a v. 2025.05.00.
    - Dependencia kotlin actualizada a v. 2.1.21.
    - Dependencia lifecycle-livedata-ktx actualizada a v. 2.9.0.
    - Dependencia lifecycle-runtime-ktx actualizada a v. 2.9.0.
    - Dependencia lifecycle-viewmodel-ktx actualizada a v. 2.9.0.
    - Dependencia navigation-fragment-ktx actualizada a v. 2.9.0.
    - Dependencia navigation-ui-ktx actualizada a v. 2.9.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,12 +57,15 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(platform(libs.androidx.compose.bom))
+    kapt(libs.androidx.room.compiler)
     // Test implementation
     testImplementation(libs.junit)
 }
@@ -71,4 +74,5 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
 }
